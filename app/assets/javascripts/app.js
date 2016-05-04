126c5195f9b281ab77b4512521470b8287cdd3ac
vagrant <vagrant@precise32.(none)>
2016-05-04 01:43:45 +0000

    tweaked some css for mobile view/added scroll animate to about

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,4 +2,11 @@ $(function(){
   $('.close').click(function(){
     $('.alert').addClass('hide')
   })
+  $(function() {
+    var offset = parseInt($("#description-container").offset().top);
+    $(".about").click(function() {
+      console.log(offset)
+      $("html,body").animate({scrollTop: offset + (offset * 0.1)}, 700);
+    })
+  })
 })
